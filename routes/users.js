fd9d0151422431cd2e5b2275b8f3a2ce0bad27bb
Erick <erick.s.souza@ufv.br>
2025-01-08 16:58:36 -0300

    Corrigindo rotas

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    res.render('index');
+});
+
+/* GET support page. */
+router.get('/suporte', function(req, res, next) {
+    res.render('support');
+});
+
+/* GET register page. */
+router.get('/cadastrar', function(req, res, next) {
+    res.render('register');
+});
+
+/* GET login page. */
+router.get('/entrar', function(req, res, next) {
+    res.render('login');
+});
+
+/* GET orders page. */
+router.get('/meus-pedidos', function(req, res, next) {
+    res.render('orders');
+});
+
+/* GET profile page. */
+router.get('/meu-perfil', function(req, res, next) {
+    res.render('profile');
 });
 
 module.exports = router;
