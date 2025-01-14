bcd0d48c7b6f22fef95801860aa45eb42a8acd1e
juliohfr <julio.h.rodrigues@ufv.br>
2025-01-13 22:16:55 -0300

    tela  "Meu perfil" pronta

---
+++ public/javascripts/profile.js
@@ -0,0 +1,15 @@
+document.getElementById('info-tab').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('info-section').style.display = 'block';
+    document.getElementById('password-section').style.display = 'none';
+    this.classList.add('active');
+    document.getElementById('password-tab').classList.remove('active');
+});
+
+document.getElementById('password-tab').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('info-section').style.display = 'none';
+    document.getElementById('password-section').style.display = 'block';
+    this.classList.add('active');
+    document.getElementById('info-tab').classList.remove('active');
+});
